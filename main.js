1bd115054056749c3068c5f9ee98046bb2d18aad
Kawsar <kawsarsdev@gmail.com>
2023-03-17 23:18:53 +0600

    footer design

---
+++ main.js
@@ -0,0 +1,32 @@
+const nav = document.querySelector("nav");
+
+
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 60) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
